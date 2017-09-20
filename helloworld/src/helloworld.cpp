b39452d8000e8cb11d1bc00f6a6f2e015b0d25a0
Andrei Mironenko <andrei.mironenko@paralleldynamic.com>
2017-09-20 10:53:15 +0100

    Getting variadic templated function

--- helloworld/src/helloworld.cpp
+++ helloworld/src/helloworld.cpp
@@ -1,8 +1,61 @@
 #include<iostream>
+#include<tuple>
 
+using namespace std;
+
+#define LIST A,B,C,D
+#define STRING_TYPES  string, string, string, string
+
+
+enum STRINGS
+{
+	LIST
+};
+
+
+std::tuple<STRING_TYPES>tp = std::make_tuple<STRING_TYPES>("A", "B", "C", "D" );
+
+void print() {
+    cout << endl;
+}
+
+
+template <typename T> void print(const T& t) {
+	cout << t << endl;
+
+	//cout << std::get<0>(tp) << endl;
+}
+
+
+template <typename First, typename... Rest> void print(const First& first, const Rest&... rest) {
+    cout << first << ", ";
+	//cout << std::get<first>(tp) << ", ";
+
+    print(rest...); // recursive call using pack expansion syntax
+}
+
+template <typename T> void foreach(const T& t) {
+	cout << std::get<t>(tp) << endl;
+}
+
+
+template <typename First, typename... Rest> void foreach(const First& first, const Rest&... rest) {
+    //cout << first << ", ";
+	cout << std::get<first>(tp) << ", ";
+	foreach(rest...);
+    //print(rest...); // recursive call using pack expansion syntax
+}
 int main() {
-    using namespace std;
-    cout << "Hello word" << endl;
-    return 0;
+#if 0
+	//print(); // calls first overload, outputting only a newline
+	print(1); // calls second overload
+
+	// these call the third overload, the variadic template,
+	// which uses recursion as needed.
+	print(10, 20);
+	print(100, 200, 300);
+	print("first", 2, "third", 3.14159);
+#endif
+	foreach(LIST);
 }
 
